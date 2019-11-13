2f9cc34e4d1bd109976b25996bae3ee3ea053dfa
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-13 15:14:03 -0500

    removed residents, source api doesn't have enough info to warrant a resident key

--- models/Location.js
+++ models/Location.js
@@ -1,14 +1,11 @@
 const mongoose = require("../db/connection");
 const Schema = mongoose.Schema;
-const People = require("./People");
-const Film = require("./Film");
 
 const LocationSchema = new Schema({
   id: String,
   name: String,
   climate: String,
   terrain: String,
-  residents: [People.schema],
   films: String,
   url: [String]
 });
