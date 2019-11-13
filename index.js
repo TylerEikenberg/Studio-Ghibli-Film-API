266efcb61a025efe6bd5ecba0decbce40680d98f
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-13 10:02:07 -0500

    added create route for people

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 const express = require("express");
 const app = express();
+const parser = require("body-parser");
 const Film = require("./models/Film");
 const People = require("./models/People");
+app.use(parser.json());
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.redirect("/films");
 });
 
-app.get("/films", function(req, res) {
+app.get("/films", (req, res) => {
   Film.find({}).then(films => {
     res.json(films);
   });
 });
 
-app.get("/films/:id", function(req, res) {
+app.get("/films/:id", (req, res) => {
   let filmId = req.params.id;
   Film.findOne({ id: filmId }).then(films => {
     res.json(films);
   });
 });
 
-app.get("/people", function(req, res) {
+app.get("/people", (req, res) => {
   People.find({}).then(people => {
     res.json(people);
   });
 });
-app.get("/people/:id", function(req, res) {
+app.get("/people/:id", (req, res) => {
   let peopleId = req.params.id;
   People.findOne({ id: peopleId }).then(people => {
+    res.json(people);
+  });
+});
+app.post("/people/post", (req, res) => {
+  People.create(req.body).then(people => {
     res.json(people);
   });
 });
@@ -35,6 +42,11 @@ app.get("/people/:id", function(req, res) {
 app.delete("/films/:id", (req, res) => {
   Film.findOneAndRemove({ id: req.params.id }).then(film => {
     res.json(film);
+  });
+});
+app.delete("/people/:id", (req, res) => {
+  People.findOneAndRemove({ id: req.params.id }).then(people => {
+    res.json(people);
   });
 });
 
