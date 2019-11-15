17174ec88b0dcd969518877c68b1d5bc8e846adf
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-14 21:00:45 -0500

    adding comments for apidocs

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(parser.json());
 app.use(express.static("doc"));
 
 /**
- * @api {get} /films Returns Studio Ghibli Movies
+ * @api {get} /films 1. Returns Studio Ghibli Movies
  * @apiName GetFilms
  * @apiGroup Films
  *
@@ -19,8 +19,8 @@ app.use(express.static("doc"));
  * @apiSuccess {String} director  Director of film.
  * @apiSuccess {String} producer  Producer of film.
  * @apiSuccess {String} release_date  Release date of film.
- * @apiSuccess {Array} people  Array of characters in the film.
- * @apiSuccess {Array} locations  Array of locations within the film.
+ * @apiSuccess {People[]} people  Array of characters in the film.
+ * @apiSuccess {Locations[]} locations  Array of locations within the film.
  */
 app.get("/films", (req, res) => {
   Film.find({}).then(films => {
@@ -29,11 +29,11 @@ app.get("/films", (req, res) => {
 });
 
 /**
- * @api {get} /films/:id Return single movie by a unique id.
- * @apiName Get Films
+ * @api {get} /films/:id 2. Return single movie by a unique id.
+ * @apiName GetFilmsId
  * @apiGroup Films
  *
- * @apiParam {String} id User's unique ID.
+ * @apiParam {String} id Films's unique ID.
  *
  * @apiSuccess {String} id Unique ID of film.
  * @apiSuccess {String} title  Title of film.
@@ -41,25 +41,104 @@ app.get("/films", (req, res) => {
  * @apiSuccess {String} director  Director of film.
  * @apiSuccess {String} producer  Producer of film.
  * @apiSuccess {String} release_date  Release date of film.
- * @apiSuccess {Array} people  Array of characters in the film.
- * @apiSuccess {Array} locations  Array of locations within the film.
+ * @apiSuccess {People[]} people  Array of characters in the film.
+ * @apiSuccess {Locations[]} locations  Array of locations within the film.
+ * 
+ * @apiSuccessExample Successful Response
+ *     HTTP/1.1 200 OK
+ * {
+ *"id": "ebbb6b7c-945c-41ee-a792-de0e43191bd8",
+  "title": "Porco Rosso",
+  "description": "Porco Rosso, known in Japan as Crimson Pig (Kurenai no Buta) is the sixth animated film by Hayao Miyazaki and released in 1992. You're introduced to an Italian World War I fighter ace, now living as a freelance bounty hunter chasing 'air pirates' in the Adriatic Sea. He has been given a curse that changed his head to that of a pig. Once called Marco Pagot, he is now known to the world as 'Porco Rosso', Italian for 'Red Pig.'",
+  "director": "Hayao Miyazaki",
+  "producer": "Toshio Suzuki",
+  "release_date": "1992",
+  "people": [
+    {
+      "films": [
+        "ebbb6b7c-945c-41ee-a792-de0e43191bd8"
+      ],
+      "_id": "5dcdf9ecf568d3021125dc5d",
+      "id": "6523068d-f5a9-4150-bf5b-76abe6fb42c3",
+      "name": "Porco Rosso",
+      "gender": "Male",
+      "url": "https://ghibli-api-tse.herokuapp.com//people/6523068d-f5a9-4150-bf5b-76abe6fb42c3",
+      "peopleUrl": "https://ghibli-api-tse.herokuapp.com//people/"
+    }
+  ],
+  "locations": [
+    {
+      "url": [
+        "https://ghibli-api-tse.herokuapp.com//locations/34df8f25-8f80-4823-8f01-bf9852039987"
+      ],
+      "_id": "5dcdf9ecf568d3021125dc5e",
+      "id": "34df8f25-8f80-4823-8f01-bf9852039987",
+      "name": "Piccolo S.P.A.",
+      "climate": "Continental",
+      "terrain": "River",
+      "films": "ebbb6b7c-945c-41ee-a792-de0e43191bd8"
+    }
+  ],
+  "url": "https://ghibli-api-tse.herokuapp.com//films/ebbb6b7c-945c-41ee-a792-de0e43191bd8",
+}
  */
 app.get("/films/:id", (req, res) => {
   Film.findOne({ id: req.params.id }).then(films => {
     res.json(films);
   });
 });
+
+/**
+ * @api {get} /films/director/:director 3. Return movies by a single director
+ * @apiName GetFilmsDirector
+ * @apiGroup Films
+ *
+ * @apiSuccess {String} id Unique ID of film.
+ * @apiSuccess {String} title  Title of film.
+ * @apiSuccess {String} description  Description of film.
+ * @apiSuccess {String} director  Director of film.
+ * @apiSuccess {String} producer  Producer of film.
+ * @apiSuccess {String} release_date  Release date of film.
+ * @apiSuccess {People[]} people  Array of characters in the film.
+ * @apiSuccess {Locations[]} locations  Array of locations within the film.
+ */
 app.get("/films/director/:director", (req, res) => {
   Film.find({ director: req.params.director }).then(films => {
     res.json(films);
   });
 });
+/**
+ * @api {get} /films/title/:title 4. Return movies by a single title
+ * @apiName GetFilmsTitle
+ * @apiGroup Films
+ *
+ * @apiSuccess {String} id Unique ID of film.
+ * @apiSuccess {String} title  Title of film.
+ * @apiSuccess {String} description  Description of film.
+ * @apiSuccess {String} director  Director of film.
+ * @apiSuccess {String} producer  Producer of film.
+ * @apiSuccess {String} release_date  Release date of film.
+ * @apiSuccess {People[]} people  Array of characters in the film.
+ * @apiSuccess {Locations[]} locations  Array of locations within the film.
+ */
 app.get("/films/title/:title", (req, res) => {
   Film.find({ title: req.params.title }).then(films => {
     res.json(films);
   });
 });
 
+/**
+ * @api {get} /people 4. Return list of characters
+ * @apiName GetCharacters
+ * @apiGroup People
+ *
+ * @apiSuccess {String} id Unique ID of character.
+ * @apiSuccess {String} name  Name of character.
+ * @apiSuccess {String} gender  Gender of character.
+ * @apiSuccess {Films[]} films  List of films character appears in.
+ * @apiSuccess {String} url  Url of unique character.
+ * @apiSuccess {String} peopleUrl Url back to /people.
+ */
 app.get("/people", (req, res) => {
   People.find({}).then(people => {
     res.json(people);
